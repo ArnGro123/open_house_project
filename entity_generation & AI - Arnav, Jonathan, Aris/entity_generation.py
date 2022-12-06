574bc26ac1c2613ccef8dcb8fca4fce997a66298
ArnGro123 <98963288+ArnGro123@users.noreply.github.com>
2022-12-05 22:31:29 -0500

    Add files via upload

--- entity_generation & AI - Arnav, Jonathan, Aris/entity_generation.py
+++ entity_generation & AI - Arnav, Jonathan, Aris/entity_generation.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 WINDOW_SIZE = (500,500)
 FPS = 30
@@ -10,11 +11,12 @@ clock = pygame.time.Clock()
 
 running = True
 
+
 class Cell:
     '''
     A class to create a single cell, used for larger entity systems and point counters
     '''
-    def __init__(self, x, y, radius, color):
+    def __init__(self, x, y, radius, color, x_speed, y_speed):
         '''
         Constructs attributes for cell creation
         '''
@@ -22,6 +24,8 @@ class Cell:
         self.y = y
         self.radius = radius
         self.color = color
+        self.x_speed = x_speed
+        self.y_speed = y_speed
     
     def draw(self):
         '''
@@ -29,25 +33,109 @@ class Cell:
         '''
         pygame.draw.circle(display, self.color, (self.x, self.y), self.radius)
 
+    
 class Entity:
     '''
-    A class to create an organic fluid entity comprising of smaller cells
+    A class to create an entity comprising of smaller cells
     '''
-    def __init__(self, x, y, cells):
+    def __init__(self, x, y, color):
         '''
         Constructs attributes for entity creation
         '''
         self.x = x
         self.y = y
-        self.cells = cells
+        self.color = color
+        self.cells = [Cell(self.x+5*i, self.y+5*j, 10, self.color, 0, 0) for j in range(3) for i in range(3)]
     
-    def spawn(self):
-        for cell in cells:
+    def draw(self):
+        '''
+        Method drawing Cell objects of Entity, and moving based on Cell velocities
+        '''
+        for cell in self.cells:
             cell.draw()
+            cell.x += cell.x_speed
+            cell.y += cell.y_speed
 
-    #TODO: add goto method
+    def goto(self, x, y):
+        '''
+        Method changing position of Entity
+        '''
 
 
+        #TODO: Fix goto method, and direction
+
+        for cell in self.cells:
+
+            x_distance = abs(cell.x-x)
+            y_distance = abs(cell.y-y)
+
+            if x_distance > 150:
+                cell.x_speed = random.randrange(5, 12)
+            elif 50 < x_distance <= 150:
+                cell.x_speed = random.randrange(1, 5)
+            elif 0 < x_distance <= 50:
+                cell.x_speed = 0.5
+            else:
+                cell.x_speed = 0 
+            
+            if y_distance > 150:
+                cell.y_speed = random.randrange(5, 12)
+            elif 50 < y_distance <= 150:
+                cell.y_speed = random.randrange(1, 5)
+            elif 0 < y_distance <= 50:
+                cell.y_speed = 0.5
+            else:
+                cell.y_speed = 0
+                
+
+    # def spawn(self):
+    #     '''
+    #     Method creating Cell objects in a list and drawing them 
+    #     '''
+        
+    #     for cell in self.cells:
+    #         cell.draw()
+    #         cell.x += cell.x_speed
+    #         cell.y += cell.y_speed
+            
+
+    # def goto(self, x, y):
+        
+    #     for cell in self.cells:
+    #         if cell.x < x:
+    #             cell.x_speed = 5
+    #             print(cell.x_speed)
+    #         else:
+    #             cell.x_speed = -5
+
+    #         if cell.y < y:
+    #             cell.y_speed = 5
+    #         else:
+    #             cell.y_speed = -5
+
+            # x_distance = abs(cell.x-x) 
+            # y_distance = abs(cell.y-y)
+
+            # if x_distance > 150:
+            #     cell.x_speed = random.randrange(5, 12)
+            # elif 50 < x_distance <= 150:
+            #     cell.x_speed = random.randrange(1, 5)
+            # elif 0 < x_distance <= 50:
+            #     cell.x_speed = 0.5
+            # else:
+            #     cell.x_speed = 0 
+            
+            # if y_distance > 150:
+            #     cell.y_speed = random.randrange(5, 12)
+            # elif 50 < y_distance <= 150:
+            #     cell.y_speed = random.randrange(1, 5)
+            # elif 0 < y_distance <= 50:
+            #     cell.y_speed = 0.5
+            # else:
+            #     cell.y_speed = 0
+
+
+player = Entity(250, 250, "red")
 
 while running == True:
     for event in pygame.event.get():
@@ -55,7 +143,10 @@ while running == True:
             pygame.quit()
             sys.exit()
 
-    
-    
+    display.fill("black")
+
+    player.draw()
+    player.goto(250, 250)
+
     pygame.display.update()
     clock.tick(FPS)
